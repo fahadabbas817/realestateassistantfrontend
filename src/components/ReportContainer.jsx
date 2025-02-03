de802ca4b06736d70fc337dc1fd0f767cef4adda
Syed Fahad Abbas <110738135+fahadabbas817@users.noreply.github.com>
2025-02-03 10:06:26 +0500

    inshaAllah

--- src/components/ReportContainer.jsx
+++ src/components/ReportContainer.jsx
@@ -4,25 +4,128 @@ import ReactMarkdown from "react-markdown";
 const ReportContainer = ({ data }) => {
   return (
     <div className="max-w-3xl mx-auto p-6 bg-transparent  space-y-6">
-     
       <section className="">
-        
-        <ReactMarkdown className="prose prose-lg">{data.Current_apartment_details}</ReactMarkdown>
+        <ReactMarkdown
+          components={{
+            h1: ({ children }) => (
+              <h1 className="text-3xl text-cyan-300 font-bold mb-2">
+                {children}
+              </h1>
+            ),
+            h2: ({ children }) => (
+              <h2 className="text-xl text-cyan-300 font-semibold mb-2">
+                {children}
+              </h2>
+            ),
+            h3: ({ children }) => (
+              <h3 className="text-lg text-cyan-300 font-medium mb-2">
+                {children}
+              </h3>
+            ),
+
+            a: ({ children }) => (
+              <a target="_blank" className="text-cyan-600 hover:underline">
+                {children}
+              </a>
+            ),
+          }}
+          className="prose prose-lg"
+        >
+          {data.Current_apartment_details}
+        </ReactMarkdown>
       </section>
 
-    
       <section className="">
-        <ReactMarkdown className="prose prose-lg">{data.Comparison}</ReactMarkdown>
+        <ReactMarkdown
+          components={{
+            h1: ({ children }) => (
+              <h1 className="text-3xl text-cyan-300 font-bold mb-2">
+                {children}
+              </h1>
+            ),
+            h2: ({ children }) => (
+              <h2 className="text-xl text-cyan-300 font-semibold mb-2">
+                {children}
+              </h2>
+            ),
+            h3: ({ children }) => (
+              <h3 className="text-lg text-cyan-300 font-medium mb-2">
+                {children}
+              </h3>
+            ),
+
+            a: ({ children }) => (
+              <a target="_blank" className="text-cyan-600 hover:underline">
+                {children}
+              </a>
+            ),
+          }}
+          className="prose prose-lg"
+        >
+          {data.Comparison}
+        </ReactMarkdown>
       </section>
 
-     
       <section className="">
-        <ReactMarkdown className="prose prose-lg">{data.Summary}</ReactMarkdown>
+        <ReactMarkdown
+          components={{
+            h1: ({ children }) => (
+              <h1 className="text-3xl text-cyan-300 font-bold mb-2">
+                {children}
+              </h1>
+            ),
+            h2: ({ children }) => (
+              <h2 className="text-xl text-cyan-300 font-semibold mb-2">
+                {children}
+              </h2>
+            ),
+            h3: ({ children }) => (
+              <h3 className="text-lg text-cyan-300 font-medium mb-2">
+                {children}
+              </h3>
+            ),
+
+            a: ({ children }) => (
+              <a target="_blank" className="text-cyan-600 hover:underline">
+                {children}
+              </a>
+            ),
+          }}
+          className="prose prose-lg"
+        >
+          {data.Summary}
+        </ReactMarkdown>
       </section>
 
-      
       <section className="">
-        <ReactMarkdown className="prose prose-lg">{data.Recommendations}</ReactMarkdown>
+        <ReactMarkdown
+          components={{
+            h1: ({ children }) => (
+              <h1 className="text-3xl text-cyan-300 font-bold mb-2">
+                {children}
+              </h1>
+            ),
+            h2: ({ children }) => (
+              <h2 className="text-xl text-cyan-300 font-semibold mb-2">
+                {children}
+              </h2>
+            ),
+            h3: ({ children }) => (
+              <h3 className="text-lg text-cyan-300 font-medium mb-2">
+                {children}
+              </h3>
+            ),
+
+            a: ({ children }) => (
+              <a target="_blank" className="text-cyan-600 hover:underline">
+                {children}
+              </a>
+            ),
+          }}
+          className="prose prose-lg"
+        >
+          {data.Recommendations}
+        </ReactMarkdown>
       </section>
     </div>
   );
